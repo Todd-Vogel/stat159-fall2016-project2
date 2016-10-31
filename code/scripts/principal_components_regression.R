447151d19b4873deb84124f8e9c418586b88fa45
Bryan Alcorn <alcorn.bryan@gmail.com>
2016-10-31 11:35:42 -0700

    fixed naming errors

--- code/scripts/principal_components_regression.R
+++ code/scripts/principal_components_regression.R
@@ -1,8 +1,4 @@
-install.packages("pls")
 require("pls")
-
-#library("lars")
-#library("MASS")
 
 training_data <- read.csv(file = "../../data/training_data.csv")
 training_data <- training_data[,-1]
@@ -19,14 +15,14 @@ pcr_obj <- pcr(response ~ predictors, validation = "CV")
 
 pcr_model <- pcr_obj$validation$PRESS
 
-
+#saving our model
 sink(file = "../../data/pcr_model.txt")
 pcr_model
 sink()
 
 
 #Adding Histograms to Images
-png('../../images/CV_Errors_Ridge.png')
+png('../../images/CV_Errors_pcr.png')
 validationplot(pcr_obj, val.type = "MSEP")
 dev.off()
 
@@ -38,10 +34,10 @@ test_set <- test_set[,-12]
 test_predictors = as.matrix(test_set)
 
 test_pcr <- predict(pcr_obj, newx = test_predictors, s = "validation$PRESS", type="response")
-save(test_pcr,file =  "../../data/testing_ridge.RData")
+save(test_pcr,file =  "../../data/testing_pcr.RData")
 
 source("../functions/mse_function.R")
-MSE(test_ridge, response)
+MSE(test_pcr, response)
 
 full_data <- read.csv(file = "../../data/scaled_credit.csv")
 full_data <- full_data[,-1]
@@ -52,9 +48,8 @@ predictors <- full_data[,-12]  #everythning but Balance
 predictors <- as.matrix(predictors)
 
 #rerunning model on the full data set
-
-full_ridge = pcr(response ~ predictors, validation = "CV")
+full_pcr = pcr(response ~ predictors, validation = "CV")
 
 #getting coefficients and saving
-pcr_coef <- coef(full_ridge)
-save(ridge_coef, file = "../../data/full_coeffecients_ridge.RData")
+pcr_coef <- coef(full_pcr)
+save(pcr_coef, file = "../../data/full_coeffecients_pcr.RData")
