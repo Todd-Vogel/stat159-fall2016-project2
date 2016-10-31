b950e1ae6074b91890b146fa7ab47529067ab767
Bryan Alcorn <alcorn.bryan@gmail.com>
2016-10-30 19:27:56 -0700

    test for MSE passes

--- code/tests/all_tests.R
+++ code/tests/all_tests.R
@@ -1,6 +1,5 @@
 source("../functions/mse_function.R")
-source("../../data/ridge_model.RData") #
-load("../../data/ridge_model.RData")
+load("../../data/testing_ridge.RData")
 require("hydroGOF")
 require(testthat)
 
@@ -8,9 +7,11 @@ test_that( "MSE Test", {
   
   test_set <- read.csv(file = "../../data/test_data.csv")
   response = test_set$Balance
-  func_answe = MSE(test_ridge, response)
-  package_asnwer = mse(test_ridge, response)
-  expect_that(func_answe, equals(package_asnwer, tolerance = .1))
+  length(response)
+  length(test_ridge)
+  func_answer = MSE(test_ridge, response)
+  package_asnwer = mse(as.vector(test_ridge), response)
+  expect_that(func_answer, equals(package_asnwer, tolerance = .1))
 })
 
 
