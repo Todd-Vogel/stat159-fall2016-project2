55f8e29c02638d8f66c1026fe9d3f0a14fc607c5
Bryan Alcorn <alcorn.bryan@gmail.com>
2016-11-01 03:37:29 -0700

    ols ready for master

--- code/scripts/ordinary_least_squares.R
+++ code/scripts/ordinary_least_squares.R
@@ -1,7 +1,14 @@
 #Getting the Training Data
-training_data <- read.csv("./data/training_data.csv")
+training_data <- read.csv("../../data/training_data.csv")
 training_data <- training_data[,-1]
 
 ols <- lm(Balance ~ Income + Limit + Rating + Cards + Age + Education + GenderFemale + StudentYes + MarriedYes + EthnicityAsian + EthnicityCaucasian, training_data)
 
 summary(ols)
+
+ols_coef <- coef(ols)
+
+source("../functions/mse_function.R")
+MSE_ols <- MSE(test_plsr, test_response)
+
+save(ols_coef, file = "../../data/ols_coef.RData")
